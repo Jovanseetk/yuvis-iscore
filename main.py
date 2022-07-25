4e09864986f87b959b150b8f595be7c3ead8443b
Jovanseetk <73521844+Jovanseetk@users.noreply.github.com>
2022-07-26 02:47:45 +0800

    added a debug mode for testing of sensors and led

--- main.py
+++ main.py
@@ -1,24 +1,55 @@
-# credits
-
-def on_button_pressed_ab():
-    basic.show_string("code written by jovan see :)")
-input.on_button_pressed(Button.AB, on_button_pressed_ab)
-
 # boot icon
 basic.show_icon(IconNames.HAPPY)
 # reads the value from the moisture sensor (analog pin 1) and assigns it to the moisture_sensor_reading
 moisture_sensor_reading = pins.analog_read_pin(AnalogPin.P1)
-# forever loop
+# sets mode value to 0 (default mode)
+mode = 0
+# clears led screen
+basic.clear_screen()
+# controls
 
 def on_forever():
-    global moisture_sensor_reading
-    # rereads the value from the moisture sensor (analog pin 1) and reassigns it to the moisture_sensor_reading
-    moisture_sensor_reading = pins.analog_read_pin(AnalogPin.P1)
-    # compares the reading to a fixed value for moisture (511)
-    if moisture_sensor_reading == 511:
-        # turns on led (digital pin 0) by setting its value to 1
-        pins.digital_write_pin(DigitalPin.P0, 1)
-    else:
-        # waits awhile (2 secs) before repeating the loop
-        basic.pause(2000)
+    global mode
+    # credits
+    if input.button_is_pressed(Button.A):
+        basic.show_string("code written by jovan see :)")
+    if input.button_is_pressed(Button.AB):
+        # sets mode value to 1 (debug mode)
+        mode = 1
 basic.forever(on_forever)
+
+# forever loop
+
+def on_forever2():
+    global moisture_sensor_reading, mode
+    # forever loop for default mode
+    # checks for default mode value
+    while mode == 0:
+        # rereads the value from the moisture sensor (analog pin 1) and reassigns it to the moisture_sensor_reading
+        moisture_sensor_reading = pins.analog_read_pin(AnalogPin.P1)
+        # compares the reading to a fixed value for moisture (511)
+        if moisture_sensor_reading == 511:
+            # turns on led (digital pin 0) by setting its value to 1
+            pins.digital_write_pin(DigitalPin.P0, 1)
+        else:
+            # waits awhile (3 secs) before repeating the loop
+            basic.pause(3000)
+    # checks for debug mode value (1)
+    if mode == 1:
+        # displays debug mode intro
+        basic.show_string("DEBUG MODE ENABLED")
+        # displays moisture sensor reading
+        basic.show_string("the moisture sensor reading is: " + str(pins.analog_read_pin(AnalogPin.P0)))
+        # turns led (digital pin 0) off
+        pins.digital_write_pin(DigitalPin.P0, 0)
+        # shows led off message
+        basic.show_string("the led is currently set to OFF")
+        # turns led (digital pin 0) on
+        pins.digital_write_pin(DigitalPin.P0, 1)
+        # shows led on message
+        basic.show_string("the led is currently set to ON")
+        # waits awhile (3 secs) before repeating the loop
+        basic.pause(3000)
+        # sets mode value to 0 (default mode)
+        mode = 0
+basic.forever(on_forever2)
--- main.ts
+++ main.ts
@@ -1,21 +1,61 @@
-// credits
-input.onButtonPressed(Button.AB, function () {
-    basic.showString("code written by jovan see :)")
-})
-// boot icon
+//  boot icon
 basic.showIcon(IconNames.Happy)
-// reads the value from the moisture sensor (analog pin 1) and assigns it to the moisture_sensor_reading
+//  reads the value from the moisture sensor (analog pin 1) and assigns it to the moisture_sensor_reading
 let moisture_sensor_reading = pins.analogReadPin(AnalogPin.P1)
-// forever loop
-basic.forever(function () {
-    // rereads the value from the moisture sensor (analog pin 1) and reassigns it to the moisture_sensor_reading
-    moisture_sensor_reading = pins.analogReadPin(AnalogPin.P1)
-    // compares the reading to a fixed value for moisture (511)
-    if (moisture_sensor_reading == 511) {
-        // turns on led (digital pin 0) by setting its value to 1
-        pins.digitalWritePin(DigitalPin.P0, 1)
-    } else {
-        // waits awhile (3 secs) before repeating the loop
-        basic.pause(3000)
+//  sets mode value to 0 (default mode)
+let mode = 0
+//  clears led screen
+basic.clearScreen()
+//  controls
+basic.forever(function on_forever() {
+    
+    //  credits
+    if (input.buttonIsPressed(Button.A)) {
+        basic.showString("code written by jovan see :)")
     }
+    
+    if (input.buttonIsPressed(Button.AB)) {
+        //  sets mode value to 1 (debug mode)
+        mode = 1
+    }
+    
+})
+//  forever loop
+basic.forever(function on_forever2() {
+    
+    //  forever loop for default mode
+    //  checks for default mode value
+    while (mode == 0) {
+        //  rereads the value from the moisture sensor (analog pin 1) and reassigns it to the moisture_sensor_reading
+        moisture_sensor_reading = pins.analogReadPin(AnalogPin.P1)
+        //  compares the reading to a fixed value for moisture (511)
+        if (moisture_sensor_reading == 511) {
+            //  turns on led (digital pin 0) by setting its value to 1
+            pins.digitalWritePin(DigitalPin.P0, 1)
+        } else {
+            //  waits awhile (3 secs) before repeating the loop
+            basic.pause(3000)
+        }
+        
+    }
+    //  checks for debug mode value (1)
+    if (mode == 1) {
+        //  displays debug mode intro
+        basic.showString("DEBUG MODE ENABLED")
+        //  displays moisture sensor reading
+        basic.showString("the moisture sensor reading is: " + ("" + pins.analogReadPin(AnalogPin.P0)))
+        //  turns led (digital pin 0) off
+        pins.digitalWritePin(DigitalPin.P0, 0)
+        //  shows led off message
+        basic.showString("the led is currently set to OFF")
+        //  turns led (digital pin 0) on
+        pins.digitalWritePin(DigitalPin.P0, 1)
+        //  shows led on message
+        basic.showString("the led is currently set to ON")
+        //  waits awhile (3 secs) before repeating the loop
+        basic.pause(3000)
+        //  sets mode value to 0 (default mode)
+        mode = 0
+    }
+    
 })
